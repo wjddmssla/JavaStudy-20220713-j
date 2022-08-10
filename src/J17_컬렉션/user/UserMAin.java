47b2f5129132bcc47db5b84aed37983c0160d254
tpqxl0088 <pakrose0108@naver.com>
2022-08-10 12:29:54 +0900

    map, set

---
+++ src/J17_컬렉션/user/UserMAin.java
@@ -0,0 +1,46 @@
+package J17_컬렉션.user;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UserMAin {
+	public static Map<String, String> addUser() {
+		Scanner scanner = new Scanner(System.in);
+		
+		Map<String, String> user = new HashMap<String, String>();
+		System.out.print("아이디: ");
+		user.put("username", scanner.nextLine());
+		
+		System.out.print("비밀번호: ");
+		user.put("password", scanner.nextLine());
+		
+		System.out.print("이름: ");
+		user.put("name", scanner.nextLine());
+		
+		return user;
+
+	}
+
+	public static void main(String[] args) {
+		Set<Map<String, String>> userSet = new HashSet<Map<String, String>>();
+		
+		userSet.add(addUser());
+		System.out.println();
+		userSet.add(addUser());
+		System.out.println();
+		userSet.add(addUser());
+		System.out.println();
+		
+		userSet.forEach(map -> {
+			System.out.println("아이디: " + map.get("username"));
+			System.out.println("비밀번호: " + map.get("password"));
+			System.out.println("이름: " + map.get("name"));
+			System.out.println();
+		});
+
+	}
+	
+}
